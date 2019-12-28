3391597ddd316863fea5abc0e30d5391aad1f27a
Aaron White <aaron@aaron.local>
2019-12-27 21:11:32 -0800

    Removed global secondar index for local local table creation

--- dynamodb/create-places-table.sh
+++ dynamodb/create-places-table.sh
@@ -1,6 +1,5 @@
 aws dynamodb create-table --table-name places \
 --endpoint-url http://localhost:8000 \
---attribute-definitions AttributeName=placeKey,AttributeType=S AttributeName=countryCode,AttributeType=S \
+--attribute-definitions AttributeName=placeKey,AttributeType=S \
 --key-schema KeyType=HASH,AttributeName=placeKey \
---global-secondary-indexes 'IndexName=countryCodeIndex,KeySchema=[{AttributeName=countryCode,KeyType=HASH}],ProvisionedThroughput={ReadCapacityUnits=1,WriteCapacityUnits=1},Projection={ProjectionType=ALL}' \
 --provisioned-throughput ReadCapacityUnits=2,WriteCapacityUnits=1
